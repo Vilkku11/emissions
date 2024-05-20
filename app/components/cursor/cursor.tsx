fe15c33c7338423ab5015f22b734f48a3f26ff3b
William Reima <william.reima@gmail.com>
2024-05-20 23:53:34 +0300

    cursor component

---
+++ app/components/cursor/cursor.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+import "./cursor.css";
+
+interface MousePosition {
+  x: number;
+  y: number;
+}
+
+interface Transition {
+  duration: number;
+  ease: string;
+}
+
+interface Variant {
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  offset: number;
+  transition?: Transition;
+}
+
+const Cursor = () => {
+  const [mousePosition, setMousePosition] = useState<MousePosition>({
+    x: 0,
+    y: 0,
+  });
+
+  const variants: { default: Variant } = {
+    default: {
+      x: mousePosition.x - 32,
+      y: mousePosition.y - 32,
+      height: 64,
+      width: 64,
+      offset: 32,
+      transition: {
+        duration: 0,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  useEffect(() => {
+    const mouseMove = (event: MouseEvent) => {
+      setMousePosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  });
+  return (
+    <motion.div
+      className="cursor"
+      variants={variants}
+      animate={"default"}
+      style={{ offset: 0 }}
+    />
+  );
+};
+
+export default Cursor;
